20bf26e20c27a5ba9ab490c6898a007ef76552ad
adnikolaeva <81370742+adnikolaeva@users.noreply.github.com>
2021-04-11 02:15:21 +0500

    1 редакция

---
+++ Dll2/Dll2.cpp
@@ -0,0 +1,31 @@
+#include "pch.cpp"
+#include "pch.h"
+#include "Dll2.h"
+#include "string.h"
+
+int Group()
+{
+	int Group = 561220;
+		return Group;
+		
+}
+
+void Name(char* name1, int n)
+{
+
+strcpy_s(name1, n, "hello");
+}
+
+//void Name(char* name1)
+//{
+//int n = 50;
+//char* str1 = new char[n];
+//strcpy_s(name1, n, "hello");
+//}
+
+//void MyName(const char* name[20])
+//{
+	//	*name = "Anna";
+		
+//}
+
---
+++ Dll2/Dll2.h
@@ -0,0 +1,3 @@
+#pragma once
+extern "C" __declspec (dllexport) int Group();
+extern "C" __declspec (dllexport) void MyName(const char* name[20]);
